7df60d7b90d7306bc900dcd9fc18d35326b5ec46
ARTEN-max <arlnu@uwaterloo.ca>
2026-03-29 14:13:39 -0400

    fix(mobile): remove workspace dep and commit shared dist for eas build

---
+++ packages/shared/dist/models/recording.js
@@ -0,0 +1,16 @@
+import {
+  recordingDetailSchema,
+  recordingSummarySchema,
+  toRecordingDetail,
+  toRecordingSummary,
+  transcriptSegmentDetailSchema
+} from "../chunk-6ADLA7ZV.js";
+import "../chunk-EW7TT2XP.js";
+export {
+  recordingDetailSchema,
+  recordingSummarySchema,
+  toRecordingDetail,
+  toRecordingSummary,
+  transcriptSegmentDetailSchema
+};
+//# sourceMappingURL=recording.js.map
